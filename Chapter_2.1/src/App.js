dbc8f783fb16d79617ac3b0e6e1be06e41c1e34f
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-04-10 12:50:30 +0530

    chapter_2_1 Commit

---
+++ Chapter_2.1/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import {Video,Thumb, Picc} from './components/Video';
+
+function App() {
+  return (
+    <div>
+      <div>Hello World!</div>
+      <Video />
+      <Thumb />
+      <Picc />
+    </div>
+
+
+  )
+}
+
+
+export default App;
---
+++ Chapter_2.1/src/components/Video.js
@@ -0,0 +1,27 @@
+function Video() {
+
+  return <div>Video</div>
+
+}
+function Thumb() {
+
+  return <div>Thumb</div>
+}
+
+  function Picc() {
+
+    return (
+
+      <div>
+        <h1>Hedy Lamarr's Todos</h1>
+        <img
+          src="https://i.imgur.com/yXOvdOSs.jpg"
+          alt="Hedy Lamarr"
+          className="photo"
+        />
+      </div>
+
+    );
+
+  }
+  export { Video, Thumb, Picc}

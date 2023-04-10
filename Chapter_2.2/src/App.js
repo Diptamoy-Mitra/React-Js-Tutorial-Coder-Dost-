9c3758d67e43ae2f9d50dea6a9f3341930f9dba1
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-04-10 13:16:24 +0530

    chapter_2_2 Commit

---
+++ Chapter_2.2/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import {Video} from './components/Video';
+
+function App() {
+  return (
+    <div>
+      <div>Hello World!</div>
+      <Video />
+     
+    </div>
+
+
+  )
+}
+
+
+export default App;

e81d72b48aaf6afd0b05f03ddf876725826a28f3
diptamoy mitra <mitradiptamoy70@gmail.com>
2023-05-01 13:08:48 +0530

    Add conditional rendering with if-else

---
+++ Chapter_4.1_conditional_rendering_if_else/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import {Video} from './components/Video';
+
+function App() {
+  let obj={
+    bgColor:"red", title:"React js tutorial", views:"1000K", time:"1 year ago", channel:"coder dost" ,verified:true
+  };
+  return (
+    <div className='App'>
+      <div>Videos</div>
+      <Video {...obj}> </Video>
+      <Video verified={false}  bgColor="green" title="Node js tutorial" views="1000000K" time="3 year ago"  channel="coder dost"></Video>
+      <Video verified={true}  bgColor="green" title="mongo db tutorial" views="1K" time="3 year ago"  channel="coder dost"></Video>
+    </div>
+
+
+  )
+}
+
+
+export default App;
---
+++ Chapter_4.1_conditional_rendering_if_else/src/components/Video.js
@@ -0,0 +1,35 @@
+import './Video.css'
+
+function Video({title,bgcolor,channel,views,time,verified}) {
+  // let verified=false;
+  let channelJSX;
+  if(verified){
+    channelJSX=<div className="channel"  >{channel}✔️</div>
+  }
+  else{
+    channelJSX=<div className="channel"  >{channel}</div>
+  }
+   
+  return (
+   <div className='container'>
+   
+      <div className='pic'>
+      <img
+        src="https://picsum.photos/id/2/160/90"
+        alt="Hedy Lamarr"
+        className="photo"
+      />
+      </div>
+     
+      <div className="title"  >{title}</div>
+      {channelJSX}
+      <div className="cviews"  >
+         {views} views <span>.</span> {time}
+        </div>
+    </div>
+
+  )
+
+}
+
+export { Video }
